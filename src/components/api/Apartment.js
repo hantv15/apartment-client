9bccdd181ad3c68d7c13382c4c6d9f6acab21f8a
hantv15 <57491771+hantv15@users.noreply.github.com>
2022-03-17 19:35:27 +0700

    han fixx

--- src/components/api/Apartment.js
+++ src/components/api/Apartment.js
@@ -5,11 +5,7 @@ export const get = (id) => {
     return instance.get(url);
 }
 export const getBillDetail = (id, bill_id) => {
-<<<<<<< Updated upstream
     const url = `/apartment/${id}/finance/${bill_id}/bill-detail`;
-=======
-    const url= `/apartment/${id}/finance/${bill_id}/bill-detail`;
->>>>>>> Stashed changes
     return instance.get(url);
 }
 export const getService = (id) => {
@@ -22,10 +18,5 @@ export const getApartmentFinace = (id) => {
 }
 export const NoGetPage = (param) => {
     const url = `/apartment?${param}`;
-    return instance.get(url);
-}
-
-export const getApartmentFinace = (id) => {
-    const url = `/apartment/29/finance`;
     return instance.get(url);
 }
